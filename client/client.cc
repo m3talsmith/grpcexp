7f66c863a8e1ceff83a4d1231a215b0e5f84aa50
Michael Christenson II <m3talsmith@gmail.com>
2024-03-01 11:45:19 -0500

    Add flags to client for host and die

--- client/client.cc
+++ client/client.cc
@@ -2,9 +2,15 @@
 #include <memory>
 #include <string>
 
+#include "absl/flags/flag.h"
+#include "absl/flags/parse.h"
+
 #include <grpc++/grpc++.h>
 
 #include "numbers.grpc.pb.h"
+
+ABSL_FLAG(std::string, host, "localhost:3000", "GRPC host address");
+ABSL_FLAG(int32_t, die, 20, "Dice to roll");
 
 using grpc::Channel;
 using grpc::ClientContext;
@@ -39,14 +45,24 @@ class NumbersClient {
         std::unique_ptr<Numbers::Stub> stub_;
 };
 
-int main(int argc, char** argv) {
-    std::string server("localhost:3000");
+int main(int argc, char* argv[]) {
+    absl::ParseCommandLine(argc, argv);
+    std::string host = absl::GetFlag(FLAGS_host);
+    int32_t die = absl::GetFlag(FLAGS_die);
+
     NumbersClient client(
-        grpc::CreateChannel(server, grpc::InsecureChannelCredentials())
+        grpc::CreateChannel(host, grpc::InsecureChannelCredentials())
     );
-    int32_t dice(20);
-    int32_t number = client.GetRandom(dice);
-    std::cout << "Your d" << dice << " result is: " << number << std::endl;
+    
+    int32_t number = client.GetRandom(die);
+    if (number < 0)
+    {
+        std::cout << "An error occurred: see above output." << std::endl;
+        return 1;
+    }
+    
+
+    std::cout << "Your d" << die << " result is: " << number << std::endl;
 
     return 0;
 }
